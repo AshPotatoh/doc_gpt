4e3dfdc7e41f0503ab399e0d08c391d95e01e607
AshPotatoh <ashlespotato@gmail.com>
2023-06-19 20:36:46 -0600

    rewrote function to output to allow user to pass in a file to have the embedding created"
    "

--- get_embeddings.py
+++ get_embeddings.py
@@ -1,17 +1,19 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 import os
 import openai
-
+import subprocess
 #gets api key from environment variable
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-#loads the data from the data folder (These can be PDFs, Txt files, or any other file type that can be read by llama_index)
-documents = SimpleDirectoryReader('data').load_data()
-index = VectorStoreIndex.from_documents(documents)
+
+def get_embeddings(path, storage_path)):
+    subprocess.call(['mkdir', 'temp.data',])
+    subprocess.call(['cp', path, 'temp.data',])
+    documents = SimpleDirectoryReader('temp.data').load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(storage_path)
+    subprocess.call(['rm', '-rf', 'temp.data',])
+
+    return "Embeddings created! "
 
 
-#saves the index to the storage folder
-index.storage_context.persist()
-
-
-print("Indexing Complete!")
